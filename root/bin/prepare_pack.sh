04ffc1929a2f638f995597698041d5d890e081cd
Jeroen Vermeulen <info@jeroenvermeulen.eu>
2016-10-25 12:41:28 +0200

    Update prepare_pack.sh

--- root/bin/prepare_pack.sh
+++ root/bin/prepare_pack.sh
@@ -1,26 +1,26 @@
 #!/bin/bash
-echo "Dropping 'vagrant' MySQL db..."
+echo "==== Dropping 'vagrant' MySQL db..."
 /usr/bin/mysqladmin -f drop vagrant 2>/dev/null
-echo "Stopping Apache, PHP-FPM, MySQL..."
+echo "==== Stopping Apache, PHP-FPM, MySQL..."
 systemctl stop apache2 php7.0-fpm mysql
-echo "Updating Git + pruning history..."
+echo "==== Updating Git + pruning history..."
 git -C /data/repos/vagrant pull
 git -C /data/repos/vagrant fetch --depth=1
 git -C /data/repos/vagrant reflog expire --expire-unreachable=now --all
 git -C /data/repos/vagrant gc --aggressive --prune=all
-echo "Updating Tools..."
+echo "==== Updating Tools..."
 /root/bin/updatetools.sh
-echo "Updating OS..."
+echo "==== Updating OS..."
 apt-get update
 apt-get -y upgrade
 apt-get -y upgrade linux-generic linux-headers-generic linux-image-generic
 apt-get -y autoremove
 apt-get clean
 update-grub
-echo "Cleaning logs..."
+echo "==== Cleaning logs..."
 rm -f   /data/mysql/ib_logfile* /data/mysql_log/*
 rm -rf  /root/.history /data/vhosts/magehostdev.pro/.history
-echo "Cleaning temp files, caches and logs..."
+echo "==== Cleaning temp files, caches and logs..."
 umount  /var/lib/lxcfs
 rm -rf  /tmp/*
 rm -rf  /data/vhosts/magehostdev.pro/tmp/*
@@ -31,14 +31,14 @@ find  / -depth -name '.cache' -exec rm -rf {} \;
 find  /var/log -type f -name '*.gz' -delete
 find  /var/log -type f -exec truncate -s 0 {} \;
 find  /data/vhosts/magehostdev.pro/logs -type f -exec truncate -s 0 {} \;
-echo "Restoring empty vhost..."
+echo "==== Restoring empty vhost..."
 umount /data/vhosts/magehostdev.pro/httpdocs 2>/dev/null
 rm -f /data/vhosts/magehostdev.pro/.my.cnf
 rm -rf /data/vhosts/magehostdev.pro/httpdocs/{.??,}*
 cp -avf /data/repos/vagrant/data/vhosts/magehostdev.pro/*  /data/vhosts/magehostdev.pro/
 chown -R vagrant: /data/vhosts/magehostdev.pro
 cd /
-echo "Zero free space..."
+echo "==== Zero free space..."
 /root/bin/zero_freespace.sh
-echo "Shutting down..."
+echo "==== Shutting down..."
 poweroff
